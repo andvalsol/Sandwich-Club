2058f2eb41695a8ccf3f51b1ca8ae0493e63bee7
Andrey Valverde Solera <andrey.valverde.solera@gmail.com>
2018-05-11 16:39:26 -0600

    This is the first project on Udacity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,60 +3,138 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
-public class DetailActivity extends AppCompatActivity {
+import org.json.JSONException;
 
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity {
+    
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
-
+    
+    private TextView mAlias;
+    private TextView mPlaceOfOrigin;
+    private TextView mDescription;
+    private TextView mIngredients;
+    private ImageView mMainImage;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
-
+        
+        //Set the text views and image view
+        mAlias = findViewById(R.id.tv_alias);
+        mPlaceOfOrigin = findViewById(R.id.tv_place_of__origin);
+        mDescription = findViewById(R.id.tv_description);
+        mIngredients = findViewById(R.id.tv_ingredients);
+        mMainImage = findViewById(R.id.iv_main_image);
+        
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
-
+        
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
             return;
         }
-
+        
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich;
+        
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            sandwich = null;
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
+            
+            //Return
             return;
         }
-
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
+        
+        //Populate UI
+        populateUI(sandwich);
     }
-
+    
     private void closeOnError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
-
-    private void populateUI() {
-
+    
+    private void populateUI(Sandwich sandwich) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Set to the toolbar the name of the sandwich
+        getSupportActionBar().setTitle(sandwich.getMainName());
+        
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(mMainImage);
+        
+//        setTitle(sandwich.getMainName());
+        getSupportActionBar().setTitle(sandwich.getMainName());
+        
+        /*
+         * Proceed to verify possible empty values for the alias list and the place of origin
+         * */
+        //Sometimes there are no other names for the sandwich... verify that is not empty, if it is, make the mAlias text view as View.VISIBLE
+        List<String> aliasList = sandwich.getAlsoKnownAs();
+        if (!aliasList.isEmpty()) {
+            mAlias.setVisibility(View.VISIBLE);
+            //Append an initial space and the proper string
+            appendWithBoldLabel(mAlias, String.valueOf(appendString(sandwich.getAlsoKnownAs())));
+        }
+        String placeOfOrigin = sandwich.getPlaceOfOrigin();
+        if (!placeOfOrigin.isEmpty()) {
+            mPlaceOfOrigin.setVisibility(View.VISIBLE);
+            //Append an initial space and the proper string
+            appendWithBoldLabel(mPlaceOfOrigin, sandwich.getPlaceOfOrigin());
+        }
+        
+        //Append an initial space and the proper string
+        appendWithBoldLabel(mIngredients, String.valueOf(appendString(sandwich.getIngredients())));
+        appendWithBoldLabel(mDescription, sandwich.getDescription());
+    }
+    
+    private void appendWithBoldLabel(TextView textView, String text) {
+        SpannableStringBuilder str = new SpannableStringBuilder(textView.getText());
+        str.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, textView.getText().toString().length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        str.append(" ").append(text);
+        
+        //Append the text to the string builder
+        textView.setText(str);
+    }
+    
+    private StringBuilder appendString(List list) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(" ").append(list.get(i));
+            
+            if (i < list.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        
+        return sb;
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,76 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    /*
+    * This method gets the Json String and returns the Sandwich bean, it may throw a JsonException
+    * */
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        
+        //Create final strings with the names of each key inside the Json object
+        final String SANDWICH_NAME = "name";
+        final String SANDWICH_MAIN_NAME = "mainName";
+        final String SANDWICH_ALIAS = "alsoKnownAs";
+        final String SANDWICH_PLACE_OF_ORIGIN = "placeOfOrigin";
+        final String SANDWICH_DESCRIPTION = "description";
+        final String SANDWICH_IMAGE = "image";
+        final String SANDWICH_INGREDIENTS = "ingredients";
+        
+        //Create a new JSON object from the json string parameter
+        JSONObject sandwichJson = new JSONObject(json);
+        
+        //Get the name object which contains the mainName and the alias
+        JSONObject sandwichName = sandwichJson.getJSONObject(SANDWICH_NAME);
+        
+        //Get the mainName from the sandwichName variable
+        String mainName = sandwichName.getString(SANDWICH_MAIN_NAME);
+        
+        //Create a JSON array to get the sandwich alias, since the sandwich can have multiple names
+        JSONArray aliasJson = sandwichName.getJSONArray(SANDWICH_ALIAS);
+        
+        //Get the place of origin from the sandwich Json object
+        String placeOfOrigin = sandwichJson.getString(SANDWICH_PLACE_OF_ORIGIN);
+        
+        //Get the sandwich description from the sandwich Json object
+        String description = sandwichJson.getString(SANDWICH_DESCRIPTION);
+        
+        //Get the sandwich image from the sandwich Json object
+        String image = sandwichJson.getString(SANDWICH_IMAGE);
+        
+        //Get the sandwich list of ingredients from the sandwich Json object
+        JSONArray ingredientsJson = sandwichJson.getJSONArray(SANDWICH_INGREDIENTS);
+        
+        //Create a new Sandwich object so that we set each field from the Json object
+        //Return the sandwich object
+        return new Sandwich(
+                mainName,
+                getStringListFromJsonArray(aliasJson),
+                placeOfOrigin,
+                description,
+                image,
+                getStringListFromJsonArray(ingredientsJson)
+        );
+    }
+    
+    private static List<String> getStringListFromJsonArray(JSONArray jsonArray) throws JSONException {
+        //Create a return list so we can add each string from the jsonArray
+        List<String> returnList = new ArrayList<>();
+        
+        //Iterate through each jsonArray field and add it to the returnList
+        for (int i = 0; i < jsonArray.length(); i++) {
+            String val = jsonArray.getString(i);
+            returnList.add(val);
+        }
+        
+        //Return the list when for loop has finished
+        return returnList;
     }
 }
